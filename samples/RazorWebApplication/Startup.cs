c2fdf3a150af4d9e60740009621a89fb56a0c22c
James Ulry <jamesulry@users.noreply.github.com>
2023-11-13 11:58:31 -0500

    added razor application

---
+++ samples/RazorWebApplication/Startup.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Tokens;
+
+using Polly;
+
+using System.IdentityModel.Tokens.Jwt;
+
+namespace RazorWebApplication
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+            services.AddRazorPages();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "cookie";
+                options.DefaultChallengeScheme = "oidc";
+            })
+                .AddCookie("cookie", options =>
+                {
+                    options.Cookie.Name = "mvccode";
+
+                    options.Events.OnSigningOut = async e =>
+                    {
+                        await e.HttpContext.RevokeUserRefreshTokenAsync();
+                    };
+                })
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.Authority = "https://demo.duendesoftware.com";
+
+                    options.ClientId = "interactive.confidential.short";
+                    options.ClientSecret = "secret";
+
+                    // code flow + PKCE (PKCE is turned on by default)
+                    options.ResponseType = "code";
+                    options.UsePkce = true;
+
+                    options.Scope.Clear();
+                    options.Scope.Add("openid");
+                    options.Scope.Add("profile");
+                    options.Scope.Add("email");
+                    options.Scope.Add("offline_access");
+                    options.Scope.Add("api");
+
+                    // not mapped by default
+                    options.ClaimActions.MapJsonKey("website", "website");
+
+                    // keeps id_token smaller
+                    options.GetClaimsFromUserInfoEndpoint = true;
+                    options.SaveTokens = true;
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = "name",
+                        RoleClaimType = "role"
+                    };
+                });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
---
+++ samples/Web/Pages/Secure.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages
+{
+    public class SecureModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
